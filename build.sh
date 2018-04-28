34d6ae852197b326bd4c486f51860c8249484a0c
chance <changxunzhou@qq.com>
2018-04-28 15:24:01 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
-cd ../nginx.git;
+cd ../nginx;
 ./auto/configure --with-debug \
             --with-stream \
-            --add-module=../ngx_healthcheck_module.git
-make ;
-#make install
+            --add-module=../ngx_healthcheck_module
+make && make install
 cd -
