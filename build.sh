b2d9faaa87055dc185d4d8ed5bf430b5d37ea65e
zhouchangxun <changxunzhou@qq.com>
2018-04-30 16:35:37 +0800

    add travis CI script

--- build.sh
+++ build.sh
@@ -1,19 +1,36 @@
 #download
 version=1.14.0
-cd /home/travis/build/zhouchangxun
+echo ============download nginx code.
+cd ../ #/home/travis/build/zhouchangxun
 wget http://nginx.org/download/nginx-$version.tar.gz
-tar -zxvf nginx-$version.tar.gz
+tar -zxf nginx-$version.tar.gz
+pwd;ls
 cd nginx-$version
 
-#build
-./auto/configure --with-debug \
+echo ============apply patch
+git apply ../ngx_healthcheck_module/nginx_healthcheck_for_nginx_1.14+.patch
+
+#check dependency
+dpkg -l |grep libpcre3-dev
+dpkg -l|grep zlib1g-dev
+dpkg -l|grep openssl
+ls auto/
+echo ===========begin build nginx
+./configure --with-debug \
             --with-stream \
             --add-module=../ngx_healthcheck_module
 make
 sudo make install
 
-#start nginx
-sudo /usr/local/nginx/sbin/nginx -c /usr/local/nginx/conf/nginx.conf
-#show
+echo ===========start nginx
+sudo cp -f ../ngx_healthcheck_module/nginx.conf.example /usr/local/nginx/conf/nginx.conf
+sudo ln -s /usr/local/nginx/sbin/nginx /usr/sbin/
+sudo nginx -T
+sudo nginx -t
+sudo nginx &
+#test
 ps -ef | grep nginx
-echo execute finish
+curl localhost
+curl localhost/status
+
+echo finish!
